484145686e223cb7f0855c63a65cde5b2231733e
parkJi-yun <nmjk0123@naver.com>
2024-04-17 17:15:35 +0900

    Feat : userDTO

--- server/src/main/java/com/toy/user/dto/UserDTO.java
+++ server/src/main/java/com/toy/user/dto/UserDTO.java
@@ -1,5 +1,25 @@
 package com.toy.user.dto;
 
-public class UserDTO {
+import java.time.LocalDate;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserDTO {
+    
+    private String userId;
+    
+    private String password;
+    
+    private String nickname;
+
+    private String name;
+    
+    private String phone;
+    
+    private String email;
+    
+    private LocalDate joinDate;
 }
