0d7e9316967db4f4301bc20514f4abf8b5454e98
parkJi-yun <nmjk0123@naver.com>
2024-04-17 17:19:38 +0900

    Feat : 회원가입, 회원조회 및 페이징처리, 회원수정

--- server/src/main/java/com/toy/user/service/UserService.java
+++ server/src/main/java/com/toy/user/service/UserService.java
@@ -1,8 +1,55 @@
 package com.toy.user.service;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import com.toy.user.dto.UserDTO;
+import com.toy.user.entity.User;
+import com.toy.user.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
 public class UserService {
+	
+	private final UserRepository userRepository;
+	
+	// 회원가입
+	public User join(UserDTO userDTO) {
+
+        User user = new User();
+        user.setUserId(userDTO.getUserId());
+        user.setPassword(userDTO.getPassword());
+        user.setNickname(userDTO.getNickname());
+        user.setName(userDTO.getName());
+        user.setPhone(userDTO.getPhone());
+        user.setEmail(userDTO.getEmail());
+        user.setJoinDate(userDTO.getJoinDate());
+
+        return userRepository.save(user);
+    }
+	
+	// 조회
+	public Page<User> findPage(int page, int pageSize, String sort) {
+        Pageable pageable = PageRequest.of(page, pageSize, Sort.by(sort));
+        return userRepository.findAll(pageable);
+    }
+	
+	// 수정
+	public User updateUser(String userId, UserDTO userDTO) {
+		
+        User existingUser = userRepository.findByUserId(userId);
+
+        existingUser.setNickname(userDTO.getNickname());
+
+        return userRepository.save(existingUser);
+    }
+	
 
 }
