50cc65e39236084176c2e53ccdd68f5baf4bb2ac
parkJi-yun <nmjk0123@naver.com>
2024-04-18 15:10:35 +0900

    Refactor : @notnull 추가

--- server/src/main/java/com/toy/user/dto/UserDTO.java
+++ server/src/main/java/com/toy/user/dto/UserDTO.java
@@ -2,6 +2,8 @@ package com.toy.user.dto;
 
 import java.time.LocalDate;
 
+import javax.validation.constraints.NotNull;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,16 +11,22 @@ import lombok.Setter;
 @Setter
 public class UserDTO {
     
+	@NotNull
     private String userId;
     
+	@NotNull
     private String password;
-    
+
+	@NotNull
     private String nickname;
 
+	@NotNull
     private String name;
     
+	@NotNull
     private String phone;
     
+	@NotNull
     private String email;
     
     private LocalDate joinDate;
